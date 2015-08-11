f95674a63809e821ce945f05cc4b3f9358f87002
Mario J. Marques-Azevedo <mariojmaaz@gmail.com>
2015-08-11 03:38:36 -0300

    Correction for no pairs in class
    Correct code to check if there are pairs in classes. If class has no
    pairs, then is attributed NA

--- correlogI/correlogI.r
+++ correlogI/correlogI.r
@@ -49,6 +49,9 @@ correlogI <- function(x, dt, method = "euclidean", nc = NULL, thresh = NULL, alp
   # Correction to bicaucal test
   if(tail == "two") alpha <- alpha / 2
   
+  # Expected I
+  EI <- -(n - 1)^-1
+  
   # For each distance classes
   for(cl in 1:nc){
     # Binary weight
@@ -61,56 +64,56 @@ correlogI <- function(x, dt, method = "euclidean", nc = NULL, thresh = NULL, alp
     # Number of pais in distance class
     res$W[cl] <- sum(w)
     
-    # Moran I
-    res$I[cl] <- ((1 / res$W[cl]) * sum(w * ((x - mean(x)) %o% (x - mean(x))))) / ((1/n) * sum((x - mean(x))^2))
-    
-    # Expected I
-    EI <- -(n - 1)^-1
-    
-    # Variance
-    S1 <- (1/2) * sum((w + t(w)) ^ 2)
-    
-    S2 <- sum((apply(w, 1, sum) + apply(w, 2, sum)) ^ 2)
-    
-    b2 <- (n * sum((x - mean(x)) ^ 4)) / ((sum((x - mean(x)) ^ 2)) ^ 2)
-    
-    num1 <- n * ( (((n^2) - (3*n) + 3) * S1) - (n*S2) + (3 * (res$W[cl]^2)) )
-    num2 <- b2 * ( (((n^2) - n) * S1) - (2*n*S2) + (6 * (res$W[cl]^2)) )
-    den <- (n-1) * (n-2) * (n-3) * (res$W[cl]^2)
-    
-    res$varI[cl] <- ((num1 - num2) / den) - (EI ^ 2)
-    
-    # Correction to small n
-    if((4 * (n - sqrt(n))) < res$W[cl] & res$W[cl] <= (4 * ((2*n) - (3*sqrt(n)) + 1)))
-      kalpha <- sqrt(10 * alpha)
-    else kalpha <- 1
-    
-    # Critical Ialpha with correction to small n
-    if(tail == "two" | tail == "upper"){
-      res$Ia.up[cl] <- (kalpha * EI) + abs(qnorm(alpha, 0, 1)) * sqrt(res$varI[cl])
+    # Checking if there are pairs in distance classe
+    if(res$W[cl] != 0){
+      # Moran I
+      res$I[cl] <- ((1 / res$W[cl]) * sum(w * ((x - mean(x)) %o% (x - mean(x))))) / ((1/n) * sum((x - mean(x))^2))
+      
+      # Variance
+      S1 <- (1/2) * sum((w + t(w)) ^ 2)
+      
+      S2 <- sum((apply(w, 1, sum) + apply(w, 2, sum)) ^ 2)
+      
+      b2 <- (n * sum((x - mean(x)) ^ 4)) / ((sum((x - mean(x)) ^ 2)) ^ 2)
+      
+      num1 <- n * ( (((n^2) - (3*n) + 3) * S1) - (n*S2) + (3 * (res$W[cl]^2)) )
+      num2 <- b2 * ( (((n^2) - n) * S1) - (2*n*S2) + (6 * (res$W[cl]^2)) )
+      den <- (n-1) * (n-2) * (n-3) * (res$W[cl]^2)
+      
+      res$varI[cl] <- ((num1 - num2) / den) - (EI ^ 2)
+      
+      # Correction to small n
+      if((4 * (n - sqrt(n))) < res$W[cl] & res$W[cl] <= (4 * ((2*n) - (3*sqrt(n)) + 1)))
+        kalpha <- sqrt(10 * alpha)
+      else kalpha <- 1
+      
+      # Critical Ialpha with correction to small n
+      if(tail == "two" | tail == "upper"){
+        res$Ia.up[cl] <- (kalpha * EI) + abs(qnorm(alpha, 0, 1)) * sqrt(res$varI[cl])
+        if(seq.bonferroni == TRUE)
+          res$Ia.up.bonf[cl] <- (kalpha * EI) + abs(qnorm(alpha / cl, 0, 1)) * sqrt(res$varI[cl])
+      }
+      if(tail == "two" | tail == "lower"){
+        res$Ia.lo[cl] <- (kalpha * EI) - abs(qnorm(alpha, 0, 1)) * sqrt(res$varI[cl])
+        if(seq.bonferroni == TRUE)
+          res$Ia.lo.bonf[cl] <- (kalpha * EI) - abs(qnorm(alpha / cl, 0, 1)) * sqrt(res$varI[cl])
+      }
+      
+      # Probability
+      if(tail == "two") 
+        res$pvalue[cl] <-  2 * pnorm(res$I[cl], (kalpha * EI), sqrt(res$varI[cl]), lower.tail = ifelse(res$I[cl] >= EI, FALSE, TRUE))
+      
+      if(tail == "upper")
+        res$pvalue[cl] <- pnorm(res$I[cl], (kalpha * EI), sqrt(res$varI[cl]), lower.tail = FALSE)
+      
+      if(tail == "lower")
+        res$pvalue[cl] <- pnorm(res$I[cl], (kalpha * EI), sqrt(res$varI[cl]))
+      
+      # Significance
       if(seq.bonferroni == TRUE)
-        res$Ia.up.bonf[cl] <- (kalpha * EI) + abs(qnorm(alpha / cl, 0, 1)) * sqrt(res$varI[cl])
+        res[cl, "."] <- ifelse(res$pvalue[cl] < (ifelse(tail == "two", (alpha * 2), alpha) / cl), "*", "")
+      else res[cl, "."] <- ifelse(res$pvalue[cl] < ifelse(tail == "two", (alpha * 2), alpha), "*", "")
     }
-    if(tail == "two" | tail == "lower"){
-      res$Ia.lo[cl] <- (kalpha * EI) - abs(qnorm(alpha, 0, 1)) * sqrt(res$varI[cl])
-      if(seq.bonferroni == TRUE)
-        res$Ia.lo.bonf[cl] <- (kalpha * EI) - abs(qnorm(alpha / cl, 0, 1)) * sqrt(res$varI[cl])
-    }
-    
-    # Probability
-    if(tail == "two") 
-      res$pvalue[cl] <-  2 * pnorm(res$I[cl], (kalpha * EI), sqrt(res$varI[cl]), lower.tail = ifelse(res$I[cl] >= EI, FALSE, TRUE))
-    
-    if(tail == "upper")
-      res$pvalue[cl] <- pnorm(res$I[cl], (kalpha * EI), sqrt(res$varI[cl]), lower.tail = FALSE)
-    
-    if(tail == "lower")
-      res$pvalue[cl] <- pnorm(res$I[cl], (kalpha * EI), sqrt(res$varI[cl]))
-    
-    # Significance
-    if(seq.bonferroni == TRUE)
-      res[cl, "."] <- ifelse(res$pvalue[cl] < (ifelse(tail == "two", (alpha * 2), alpha) / cl), "*", "")
-    else res[cl, "."] <- ifelse(res$pvalue[cl] < ifelse(tail == "two", (alpha * 2), alpha), "*", "")
   }
   
   return(list(EI = EI, 
