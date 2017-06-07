f50068c6c571c193098c2ec4bfa12f38ae2260ec
Mario J. Marques-Azevedo <mariojmaaz@gmail.com>
2017-06-06 23:34:18 -0300

    Minor update in selection index

--- drop.clade.label/nodedepth.r
+++ drop.clade.label/nodedepth.r
@@ -1,11 +1,13 @@
 nodedepth <- function(tree, node){
+  # Count number of nodes of each 'node' to root of 'tree'
+  
   depth <- c()
   for(i in node){
     ct <- 0
     nb <- tree$edge[tree$edge[ ,2] == i,1]
-    if(length(nb) > 0) ct <- ct + 1
+    if(length(nb) > 0) ct <- 1
     while(length(nb) > 0){
-      nb <- tree$edge[tree$edge[ ,2] == nb,1]
+      nb <- tree$edge[tree$edge[ ,2] %in% nb,1]
       if(length(nb) > 0) ct <- ct + 1
     }
     depth <- c(depth, ct)
