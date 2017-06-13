3dd4543248d9e600863fd37d0b01eeaa32953cd2
Mario J. Marques-Azevedo <mariojmaaz@gmail.com>
2017-06-13 18:08:26 -0300

    New function to extract clade of tree with singletons
    This function extract clade of tree with singletons. It is used as
    internal call from drop.clade.label. But it can be used alone too.

---
+++ drop.clade.label/extract.clade.label.r
@@ -0,0 +1,64 @@
+extract.clade.label <- function(tree, node){
+  if(!is.vector(node, mode = "character") | length(node) > 1)
+    stop("'node' parameter must be a character vector of length 1")
+  
+  if(sum(node %in% tree$node.label) == 0)
+    stop("tree has not node labels defined in 'node' parameter")
+  
+  if(!is.rooted(tree))
+    stop("tree must be rooted")
+  
+  if(!is.null(tree$edge.length))
+    stop("tree must not have edge length")
+  
+  # Reorder tree
+  tree <- reorder(tree)
+  
+  # number of tips
+  not <- length(tree$tip.label)
+  # node number
+  nn <- which(tree$node.label %in% node) + not
+  
+  edge <- tree$edge
+  
+  # nodes descendants
+  nd <- tmp <- edge[edge[ ,1] %in% nn , 2]
+  while(length(tmp) > 0){
+    tmp <- edge[edge[ ,1] %in% tmp , 2]
+    if(length(tmp) > 0) nd <- c(nd, tmp)
+  }
+  # tips desdendants
+  td <- nd[nd <= not]
+  # only nodes descendats
+  nd <- nd[nd > not] 
+  
+  # New tips positions
+  newt <- matrix(NA, nrow = not, ncol = 2, 
+                 dimnames = list(1:not, c('tips', 'new')))
+  newt[ ,1] <- 1:not
+  newt <- newt[newt[ ,1] %in% td, ]
+  newt[ ,2] <- rank(newt[ ,1])
+  
+  # New nodes positions
+  newn <- matrix(NA, nrow = tree$Nnode, ncol = 2, 
+                 dimnames = list(1:tree$Nnode, c('nodes', 'new')))
+  newn [ ,1] <- 1:tree$Nnode + not
+  newn <- newn[newn[ ,1] %in% c(nd, nn), ]
+  newn [ ,2] <- rank(newn[ ,1]) + length(td)
+  
+  # Reference table to new nodes positions
+  refedge <- rbind(newt, newn)
+  
+  newedge <- edge[edge[ ,1] %in% c(nd, nn), ]
+  newedge[ ,1] <- refedge[match(newedge[ ,1], refedge[ ,1]) ,2]
+  newedge[ ,2] <- refedge[match(newedge[ ,2], refedge[ ,1]) ,2]
+  
+  # Update tree  
+  tree$edge <- newedge
+  tree$tip.label <- tree$tip.label[newt[ ,1]]
+  tree$node.label <- tree$node.label[newn[ ,1] - not]
+  tree$Nnode <- length(tree$node.label)
+  
+  return(tree)
+}
+
